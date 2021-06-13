09a81e3f20bb723e0a2bfeec998986e889c84f6d
HaroOrozco11103 <haro.yo51@hotmail.com>
2021-06-13 16:16:02 -0500

    new seed factorie update

--- database/factories/ParticipacionFactory.php
+++ database/factories/ParticipacionFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Participacion::class, function (Faker $faker) {
     return [
-        'dinamica_id' => $faker->numberBetween($min = 1, $max = 9),
-        'puntaje' => $faker->numberBetween($min = -1, $max = 500),
+        'dinamica_id' => $faker->numberBetween($min = 1, $max = 10),
+        'puntaje' => $faker->numberBetween($min = -1, $max = 300),
         'alumno_id' => $faker->numberBetween($min = 1, $max = 20),
     ];
 });
