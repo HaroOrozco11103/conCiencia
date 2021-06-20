e8e3f9cbe563b9a4bf3b4b861dbc2a23f29e8c79
Adan Piceno <metalgdl15@gmail.com>
2021-06-19 21:47:28 -0500

    diagrama  ajustada

--- public/js/scripts/grafica.js
+++ public/js/scripts/grafica.js
@@ -1,40 +1,55 @@
-Highcharts.chart('graficaCont', {
-  title: {
-    text: 'Gráfica de dispersión de datos con recta de regresión'
-  },
-  xAxis: {
-    min: 0,
-    max: recta[1][0]
-  },
-  yAxis: {
-    min: 0
-  },
-  series: [{
-    type: 'line',
-    name: 'Recta de regresión',
-    data: recta,
-    marker: {
-      enabled: false
+$(document).ready(function(){
+
+  Highcharts.setOptions({
+    colors: ['#50B432', '#303030', '#ff0000', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']
+   });
+   
+  Highcharts.chart('graficaCont', {
+    title: {
+      text: 'Gráfica de dispersión de datos con recta de regresión'
     },
-    states: {
-      hover: {
-        lineWidth: 0
+    xAxis: {
+      title: {
+        text: "Veces jugadas"
+      },
+      min: 0,
+      max: recta[1][0]
+    },
+    yAxis: {
+      title: {
+        text: "Puntaje"
+      },
+      min: 0
+    },
+    series: [{
+      type: 'line',
+      name: 'Recta de predicción',
+      data: recta,
+      marker: {
+        enabled: false
+      },
+      states: {
+        hover: {
+          lineWidth: 0
+        }
+      },
+      enableMouseTracking: false
+    }, {
+      type: 'scatter',
+      name: 'Datos',
+      data: scatterPlot,
+      marker: {
+        radius: 4
       }
-    },
-    enableMouseTracking: false
-  }, {
-    type: 'scatter',
-    name: 'Datos',
-    data: scatterPlot,
-    marker: {
-      radius: 4
-    }
-  }, {
-    type: 'scatter',
-    name: 'Predicción',
-    data: puntoPred,
-    marker: {
-      radius: 6
-    }
-  }]
+    }, {
+      type: 'scatter',
+      name: 'Predicción',
+      data: puntoPred,
+      marker: {
+        symbol: 'circle', 
+        radius: 4
+      }
+    }]
+  });
+  
 });
