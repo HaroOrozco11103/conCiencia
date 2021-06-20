f4be9feb8da5c68f3df6afe8a3a2e12ec80e56a5
HaroOrozco11103 <haro.yo51@hotmail.com>
2021-06-20 00:03:33 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -65909,8 +65909,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\conCiencia\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\conCiencia\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\laragon\www\conCiencia\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\laragon\www\conCiencia\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
